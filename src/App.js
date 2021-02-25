ea0cbb333d76eba647728b04733f1a4959962dd1
Gianpaolo Macerola <gmacerola@gmail.com>
2021-02-25 08:36:16 -0600

    basic cards on home working

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import "./App.css";
+import { MuiThemeProvider } from "@material-ui/core/styles";
+import createMuiTheme from "@material-ui/core/styles/createMuiTheme";
 
 // Components
 import Navbar from "./components/Navbar";
@@ -10,21 +12,34 @@ import home from "./pages/home";
 import login from "./pages/login";
 import signup from "./pages/signup";
 
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      main: "#c62828",
+    },
+    secondary: {
+      main: "#0d47a1",
+    },
+  },
+});
+
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <Router>
-          <Navbar />
-          <div className="container">
-            <Switch>
-              <Route exact path="/" component={home} />
-              <Route exact path="/login" component={login} />
-              <Route exact path="/signup" component={signup} />
-            </Switch>
-          </div>
-        </Router>
-      </div>
+      <MuiThemeProvider theme={theme}>
+        <div className="App">
+          <Router>
+            <Navbar />
+            <div className="container">
+              <Switch>
+                <Route exact path="/" component={home} />
+                <Route exact path="/login" component={login} />
+                <Route exact path="/signup" component={signup} />
+              </Switch>
+            </div>
+          </Router>
+        </div>
+      </MuiThemeProvider>
     );
   }
 }
--- src/pages/home.js
+++ src/pages/home.js
@@ -1,11 +1,42 @@
 import React, { Component } from "react";
+import axios from "axios";
+import Grid from "@material-ui/core/Grid";
+
+import Broadcast from "../components/Broadcast";
 
 class home extends Component {
+  state = {
+    broadcasts: null,
+  };
+
+  componentDidMount() {
+    axios
+      .get("/broadcasts")
+      .then((res) => {
+        this.setState({
+          broadcasts: res.data,
+        });
+      })
+      .catch((err) => console.log(err));
+  }
+
   render() {
+    let recentBroadcastsMarkup = this.state.broadcasts ? (
+      this.state.broadcasts.map((broadcast) => (
+        <Broadcast key={broadcast.broadcastId} broadcast={broadcast} />
+      ))
+    ) : (
+      <p>Loading...</p>
+    );
     return (
-      <div>
-        <h1>Home Page</h1>
-      </div>
+      <Grid container spacing={2}>
+        <Grid item sm={8} xs={12}>
+          {recentBroadcastsMarkup}
+        </Grid>
+        <Grid item sm={4} xs={12}>
+          <p>Profile</p>
+        </Grid>
+      </Grid>
     );
   }
 }
